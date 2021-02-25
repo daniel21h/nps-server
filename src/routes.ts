74505a6001905f6558ea0f9f60eaa785795c0216
daniel21h <hesseldaniel21@gmail.com>
2021-02-25 08:29:44 -0300

    feat: refactoring code using repositories

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { UsersController } from "./controllers/UsersController";
+import { SurveysController } from "./controllers/SurveysController";
 
 const routes = Router();
 
 const usersController = new UsersController();
+const surveysController = new SurveysController()
 
 routes.post('/users', usersController.create);
+routes.post('/surveys', surveysController.create);
 
 export { routes };
