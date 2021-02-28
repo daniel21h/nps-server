1419cfea9e5442859015eaeb4ef996b26587989f
daniel21h <hesseldaniel21@gmail.com>
2021-02-28 09:27:58 -0300

    feat: create nps calculate controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { UsersController } from "./controllers/UsersController";
 import { SurveysController } from "./controllers/SurveysController";
 import { SendMailController } from "./controllers/SendMailController";
 import { AnswerController } from "./controllers/AnswerController";
+import { NPSController } from "./controllers/NPSController";
 
 const routes = Router();
 
@@ -12,6 +13,8 @@ const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
 
+const npsController = new NPSController();
+
 routes.post('/users', usersController.create);
 
 routes.get('/surveys', surveysController.index);
@@ -20,5 +23,7 @@ routes.post('/surveys', surveysController.create);
 routes.post('/sendMail', sendMailController.execute);
 
 routes.get('/answers/:value', answerController.execute);
+
+routes.get('/nps/:survey_id', npsController.execute);
 
 export { routes };
