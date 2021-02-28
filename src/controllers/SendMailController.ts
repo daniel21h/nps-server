7afb2becee3d4781ffc48bf3f92757eb1b3f5b34
daniel21h <hesseldaniel21@gmail.com>
2021-02-28 08:25:50 -0300

    feat: refactoring send mail controller to id

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -38,16 +38,6 @@ class SendMailController {
     const user = userAlreadyExists;
     const survey = surveyAlreadyExists;
 
-
-
-    const variables = {
-      name: user.name,
-      title: survey.title,
-      description: survey.description,
-      user_id: user.id,
-      link: process.env.URL_MAIL
-    }
-
     const npsPath = resolve(
       __dirname,
       '..',
@@ -57,11 +47,20 @@ class SendMailController {
     );
 
     const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{user_id: user.id}, {value : null}],
+      where: {user_id: user.id, value : null},
       relations: ['user', 'survey']
     })
 
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+      id: '',
+      link: process.env.URL_MAIL
+    }
+
     if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id
       await SendMailService.execute(email, survey.title, variables, npsPath);
 
       return response.json(surveyUserAlreadyExists);
@@ -73,6 +72,8 @@ class SendMailController {
     });
 
     await surveysUsersRepository.save(surveyUser);
+
+    variables.id = surveyUser.id
 
     await SendMailService.execute(
       email,
