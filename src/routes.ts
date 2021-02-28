55c4109cf92a1af8b98213f845ed6cf1a4e7a007
daniel21h <hesseldaniel21@gmail.com>
2021-02-28 08:41:51 -0300

    feat: create controller of the users answer

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UsersController } from "./controllers/UsersController";
 import { SurveysController } from "./controllers/SurveysController";
 import { SendMailController } from "./controllers/SendMailController";
+import { AnswerController } from "./controllers/AnswerController";
 
 const routes = Router();
 
@@ -9,6 +10,7 @@ const usersController = new UsersController();
 const surveysController = new SurveysController();
 
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 routes.post('/users', usersController.create);
 
@@ -16,5 +18,7 @@ routes.get('/surveys', surveysController.index);
 routes.post('/surveys', surveysController.create);
 
 routes.post('/sendMail', sendMailController.execute);
+
+routes.get('/answers/:value', answerController.execute);
 
 export { routes };
